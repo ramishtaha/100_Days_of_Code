5f86ba854acbbc05157a22c67b36061cee2b50fa
Ramish Taha <ramishtaha1@gmail.com>
2021-02-05 10:00:20 +0530

    exercise for day_1

--- DAY_1/variables.py
+++ DAY_1/variables.py
@@ -1,3 +1,3 @@
 name = input("What is your Name?\n")
 name_len = len(name)
-print("Your Name is" , name, "It has" , name_len, "Characters")
+print("Your Name is" , name, " and It has" , name_len, "Characters")
