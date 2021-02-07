f5425d4b8be86cbe6b01e42670fd85629eab996c
Ramish Taha <ramishtaha1@gmail.com>
2021-02-07 12:25:15 +0530

    Feeling Fantastic

--- DAY_3/rollarcoster.py
+++ DAY_3/rollarcoster.py
@@ -4,7 +4,9 @@ height = int(input("Please Enter Your Height in cm:\n"))
 if height > 120:
     print("You can Ride the RollerCoster\n")
     age = int(input("Please Enter Your age in Yrs:\n"))
-    if age < 18:
+    if age < 12:
+        print("Please Pay $5.\n")
+    elif age <= 18:
         print("Please Pay $7.\n")
     else:
         print("Please Pay $12.\n")
