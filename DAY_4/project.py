2b32f7524f8ef7f7b573f88978dade1f83ffe991
Ramish Taha <ramishtaha1@gmail.com>
2021-02-07 14:54:53 +0530

    Completed Day 4

---
+++ DAY_4/project.py
@@ -0,0 +1,50 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+actions = [rock, paper, scissors]
+
+print("Welcome to the Game 'Rock Paper Scissors'\n")
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+computer_choice = random.randint(0, 2)
+print("You Choose:")
+print(actions[user_choice])
+
+print("Computer chose:")
+print(actions[computer_choice])
+
+
+
+if computer_choice == user_choice:
+    print("Draw!")
+
+else:
+    if (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+        print("You Win!")
+    else:
+        print("You Loose!")
