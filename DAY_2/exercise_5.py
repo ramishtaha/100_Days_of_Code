2f7238891c1c5ab1b8708c01ebeb4492c770f142
Ramish Taha <ramishtaha1@gmail.com>
2021-02-07 12:17:50 +0530

    another program for day 3

--- DAY_2/exercise_5.py
+++ DAY_2/exercise_5.py
@@ -3,3 +3,14 @@ print(7 - 4)
 print(3 * 2)
 print(6 / 7)
 print(2 ** 2)
+
+# PEMDAS
+
+# Paranthesis
+# Exponent
+# Multiplication
+# Division
+# Addition
+# Subtraction
+
+
---
+++ DAY_3/rollarcoster.py
@@ -0,0 +1,12 @@
+print("Welcome to the rollercoster!\n")
+height = int(input("Please Enter Your Height in cm:\n"))
+
+if height > 120:
+    print("You can Ride the RollerCoster\n")
+    age = int(input("Please Enter Your age in Yrs:\n"))
+    if age < 18:
+        print("Please Pay $7.\n")
+    else:
+        print("Please Pay $12.\n")
+else:
+    print("Sorry you have to grow Taller before you can ride.")
