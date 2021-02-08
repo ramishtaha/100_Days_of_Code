e972d60eecb6dee5f9c14de006ba277d134381b9
Ramish Taha <ramishtaha1@gmail.com>
2021-02-08 06:43:44 +0530

    Days in Month

--- DAY_10/exercise_01.py
+++ DAY_10/exercise_01.py
@@ -2,16 +2,20 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+    if is_leap(year) and month == 2:
+        return month_days[month - 1] + 1
+    else:
+        return month_days[month - 1]
   
   
 #🚨 Do NOT change any of the code below 
