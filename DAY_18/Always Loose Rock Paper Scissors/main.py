ee320bea97cfe4de68f587b20279059a3605dfeb
Ramish Taha <ramishtaha1@gmail.com>
2021-03-16 13:07:58 +0530

    Another Commit

---
+++ DAY_18/Always Loose Rock Paper Scissors/main.py
@@ -0,0 +1,12 @@
+print("Welcome to Always loose Rock Paper Scissors")
+
+inp = input("Enter Rock, Paper or Scissors\n")
+
+if inp == "Rock":
+    print("Paper, You Loose.")
+elif inp == "Paper":
+    print("Scissors, You Loose.")
+elif inp == "Scissors":
+    print("Rock, You Loose.")
+else:
+    print("Please enter correct Values")
