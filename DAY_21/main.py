bbcd528648b856c4f587f2a2edb61c528e3c2b1b
Ramish Taha <ramishtaha1@gmail.com>
2021-04-25 12:55:50 +0530

    Day 22 Complete

--- DAY_21/main.py
+++ DAY_21/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,7 +13,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-# screen.update()
+ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -22,7 +25,32 @@ screen.onkey(l_paddle.move_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
+    ball.move()
     screen.update()
-    time.sleep(0)
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Bounce
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 340:
+        # Right Paddle Misses
+        ball.reset_position()
+        scoreboard.score_left += 1
+        scoreboard.update_scoreboard()
+
+
+    if ball.xcor() < -340:
+        # Left Paddle Misses
+        ball.reset_position()
+        scoreboard.score_right += 1
+        scoreboard.update_scoreboard()
+
+
 
 screen.exitonclick()
